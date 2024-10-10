aa07b7a8a20ef17b93196c83501c9886bf32e580
abdulshafi99 <abdulshafielwahsh@gmail.com>
2024-10-10 10:38:49 +0300

    Adding Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+posts  = [
+    {
+        'author': f"Author{i}",
+        'title': f'Blog post {i}',
+        'Content': f'content of  blog post {i}',
+        'date_posted': f'April {i} 2018'
+    }
+
+    for i in range(1, 21)
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title="About")
 
 if __name__ == "__main__":
     app.run(debug=True)
